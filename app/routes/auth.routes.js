f3670fe794ef94c184ad1865d7400d8688caa72b
kiwoonjung <kiwoon0627@gmail.com>
2023-03-31 19:38:14 -0700

    added Access-Control-Allow-Origin in auth.routes.js

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -6,7 +6,9 @@ module.exports = function (app) {
   app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+      "x-access-token, Origin, Content-Type, Accept",
+      "Access-Control-Allow-Origin",
+      "http://localhost:8080"
     );
     next();
   });
