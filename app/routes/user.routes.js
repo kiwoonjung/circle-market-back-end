fe158e7ba0eb93abea6f7831cb9fcb8d52a40d63
kiwoonjung <kiwoon0627@gmail.com>
2023-04-08 21:47:13 -0700

    edied routes res headres

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -12,16 +12,14 @@ module.exports = function (app) {
     //   "Access-Control-Allow-Origin",
     //   "*"
     // );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-    res.setHeader(
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
       "Access-Control-Allow-Methods",
-      "Content-Type",
-      "Authorization"
+      "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     next();
   });
