ddf3c5b7b835a62d01e50b3df8f4be72f73611d5
kiwoonjung <kiwoon0627@gmail.com>
2023-04-08 22:24:05 -0700

    test

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ const cors = require("cors");
 const cookieSession = require("cookie-session");
 const app = express();
 
-// var corsOptions = {
-//   credentials: true,
-//   origin: [
-//     "http://localhost:3000",
-//     "http://localhost:8080",
-//     "http://localhost:8000",
-//     "https://www.circlemarket.ca",
-//     "https://api.circlemarket.ca",
-//   ],
-// };
-// app.use(cors(corsOptions));
-app.use(cors());
+var corsOptions = {
+  credentials: true,
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "https://www.circlemarket.ca",
+    "https://api.circlemarket.ca",
+  ],
+};
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.static("public"));
